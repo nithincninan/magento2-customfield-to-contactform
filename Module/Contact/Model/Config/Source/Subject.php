55d22191cb52b03d1c4535b5e2523fed67044ac5
Nithin Ninan <2525741+nithincninan@users.noreply.github.com>
2019-05-19 15:00:00 +0530

    Magento2.3 - customfield-to-contactfiorm

--- Module/Contact/Model/Config/Source/Subject.php
+++ Module/Contact/Model/Config/Source/Subject.php
@@ -35,18 +35,11 @@ class Subject implements \Magento\Framework\Option\ArrayInterface
     public function toArray()
     {
         return [
-            0 => __('Select A Subject'),
-            1 => __('General Customer Service'),
-            2 => __('Returns'),
-            3 => __('Order Status'),
-            4 => __('International Order Inquiry'),
-            5 => __('Our Products'),
-            6 => __('Shipping Question'),
-            7 => __('Combine Auctions'),
-            8 => __('Auction Inquiry'),
-            9 => __('Customer Info Change'),
-            10 => __('Website Suggestions'),
-            11 => __('Website Issues')
+            0 => __('Select Subject'),
+            1 => __('Customer Service'),
+            2 => __('Order Status'),
+            3 => __('Products'),
+            4 => __('Website Suggestions')
         ];
     }
 }
